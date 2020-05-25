458babe0f1111a402aeb5cde1a120885a62899d6
rzoro <qsam4you@gmail.com>
2020-05-25 22:09:52 +0300

    Done
    Rewrite to 1 cicles instead 4 cicles

--- code.js
+++ code.js
@@ -1,31 +1,204 @@
-const a = parseInt(prompt('Просто впишите 0'))
+function rand() {
+  let min = 1000;
+  let max = 9999;
+  let random = Math.floor(Math.random() * (+max - +min)) + +min;
+  return random
+}
 
-let arr = []
+function generateData(amount) {
+  const resultData = [];
 
-if (a === 0) {
-  arr.push('1', '2', '3', '4', '5')
+  for (let i = 0; i < amount; i++) {
+    resultData.push(rand());
+  }
+  return resultData;
+}
+
+const userInput = Number.parseInt(prompt())
+const generatedArray = generateData(userInput);
+
+// let result = rand()
+
+function mathCalc(array) {
+  let amountEven = 0;
+  let amountOdd = 0;
   let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum = sum + parseInt(arr[i]) / arr.length
-  }
-  alert(sum)
-}
-
-function calcNat(array) {
-  let index = 0;
-  const result = []
-  while (index < array.length) {
+  for (let index = 0; index < array.length; index++) {
     if (array[index] % 2 === 0) {
-      result.push(array[index])
+      amountEven++
     }
-    index++
+    else {
+      amountOdd++
+    }
+    sum = sum + array[index]
   }
-  return (result)
+  console.log(amountEven, amountOdd, sum / array.length)
 }
 
-alert(calcNat(arr))
+// function calcAverage(array) {
+//   let result = 0;
+//   for (let index = 0; index < array.length; index++) {
+//     const current = array[index]
+//     result = result + current
+//   }
+//   return result / array.length
+// }
+
+// function calcEven(array) {
+//   let result = 0;
+//   for (let index = 0; index < array.length; index++) {
+//     if (array[index] % 2 === 0) {
+//       result++
+//     }
+//   }
+//   return result
+// }
+
+// function calcOdd(array) {
+//   let result = 0;
+//   for (let index = 0; index < array.length; index++) {
+//     if (array[index] % 2 != 0) {
+//       result++
+//     }
+//   }
+//   return result
+// }
+
+// let averageNum = calcAverage(generatedArray)
+// console.log(averageNum)
+
+console.log(generatedArray)
+mathCalc(generatedArray)
 
 
+// let evenNum = calcEven(generatedArray)
+// let oddNum = calcOdd(generatedArray)
+// console.log(evenNum)
+// console.log(oddNum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let i = 0;
+// do {
+//   arr.push(rand);
+//   i++;
+// }
+// while (i < arr.length);
+// alert(i)
+
+// let i = 0;
+// do {
+//   alert( i );
+//   i++;
+// } while (i < 3);
+
+
+
+// let min = 1000;
+// let max = 9999;
+// let random = Math.floor(Math.random() * (+max - +min)) + +min;
+// arr.push(random)
+// alert(arr)
+
+// function calcNat(array) {
+//   let index = 0;
+//   const result = []
+//   while (index < array.length) {
+//     if (array[index] % 2 === 0) {
+//       result.push(array[index])
+//     }
+//     index++
+//   }
+//   return (result)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (a === 0) {
+//   arr.push('1', '2', '3', '4', '5')
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum = sum + parseInt(arr[i]) / arr.length
+//   }
+//   alert(sum)
+// }
+
+// function calcNat(array) {
+//   let index = 0;
+//   const result = []
+//   while (index < array.length) {
+//     if (array[index] % 2 === 0) {
+//       result.push(array[index])
+//     }
+//     index++
+//   }
+//   return (result)
+// }
+
+// alert(calcNat(arr))
 
 // function average(arr) {
 //   return arr.reduce(arr. / arr.length);
